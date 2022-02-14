18c6bb648349015f31e7c3f5143baf753ad488b9
johnny2136 <johnny2136@gmail.com>
2022-02-14 10:37:39 -0500

    added first block of code

--- Recursive-Fibonacci.go
+++ Recursive-Fibonacci.go
@@ -1,1 +1,24 @@
+package main
 
+import (
+	"fmt"
+	"time"
+)
+
+// fibonacci in GOlang good to 45 places
+// fibo function returns Recursive Solution
+func fiboRec(n int) int {
+	if n < 2 {
+		return n
+	}
+	return fiboRec(n-1) + fiboRec(n-2)
+}
+
+func main() {
+	start := time.Now()
+
+	for i := 0; i < 25; i++ {
+		fmt.Println(fiboRec(i))
+	}
+	fmt.Println(time.Since(start))
+}
